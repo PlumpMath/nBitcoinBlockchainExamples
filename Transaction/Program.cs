2f0344b12b6c067616458a7e9188f71b3b04263d
varsnotwars <luiss@hush.ai>
2017-11-21 21:36:54 +1100

    Created function to query for transaction

--- Transaction/Program.cs
+++ Transaction/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NBitcoin;
+using QBitNinja.Client;
+using QBitNinja.Client.Models;
 
 namespace Transaction
 {
@@ -10,6 +13,32 @@ namespace Transaction
     {
         private static void Main(string[] args)
         {
+            var transaction = GetTxResponse().GetAwaiter();
+            var result = transaction.GetResult();
+
+
+            Console.WriteLine(transaction);
+
+            // get NBitcoin.Transaction type
+            var nBitcoinTransaction = result.Transaction;
+
+            Console.WriteLine(nBitcoinTransaction);
+
+
+        }
+
+        private static async Task<GetTransactionResponse> GetTxResponse()
+        {
+            // create client
+            var client = new QBitNinjaClient(Network.Main);
+
+            // parse tx id to NBitcoin.uint256 for client
+            var transactionId = uint256.Parse("f13dc48fb035bbf0a6e989a26b3ecb57b84f85e0836e777d6edf60d87a4a2d94");
+
+            // query tx
+            var transactionResponse = await client.GetTransaction(transactionId);
+
+            return transactionResponse;
         }
     }
 }
