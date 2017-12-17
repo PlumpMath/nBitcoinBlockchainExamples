c2a0201056595c1e67eb75d319d0757a1a12b270
varsnotwars <luiss@hush.ai>
2017-12-17 23:58:09 +1100

    Working on P2PK example

--- OtherTypesOfOwnership/Program.cs
+++ OtherTypesOfOwnership/Program.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NBitcoin;
 
 namespace OtherTypesOfOwnership
 {
     internal class Program
     {
         private static void Main(string[] args)
-
         {
+            // P2PK[H] (pay to public key [hash])
+
+            var publicKeyHash = new Key().PubKey.Hash;
+            var bitcoinAddress = publicKeyHash.GetAddress(Network.Main);
+
+            // a bitcoin address is a hash of a public key
+            Console.WriteLine(publicKeyHash);
+            Console.WriteLine(bitcoinAddress);
+
+            // technically, there is no such thing as a "bitcoin address"
+            // the blockchain identifies a receiver with a ScriptPubKey
+            // the ScriptPubKey could be generated from the adddress
+
+            var scriptPubKey = bitcoinAddress.ScriptPubKey;
+            Console.WriteLine(scriptPubKey);
+
+            // and vice versa
+            var sameBitconAddress = scriptPubKey.GetDestinationAddress(Network.Main);
+            Console.WriteLine(bitcoinAddress == sameBitconAddress);
         }
     }
 }
