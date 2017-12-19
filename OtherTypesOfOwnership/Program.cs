a90ce6fc815520df098e1956f59906b41c9dda28
varsnotwars <luiss@hush.ai>
2017-12-19 23:56:34 +1100

    Working on multi sig example

--- OtherTypesOfOwnership/Program.cs
+++ OtherTypesOfOwnership/Program.cs
@@ -11,7 +11,9 @@ namespace OtherTypesOfOwnership
     {
         private static void Main(string[] args)
         {
+            ////
             // P2PKH (pay to public key hash) - private key -> public key -> pubilc key hash -> script pub key
+            ////
 
             var publicKeyHash = new Key().PubKey.Hash;
             var bitcoinAddress = publicKeyHash.GetAddress(Network.Main);
@@ -47,8 +49,9 @@ namespace OtherTypesOfOwnership
             Console.WriteLine($"Pay to public key: {key.PubKey.ScriptPubKey}");
             Console.WriteLine($"Pay to public key hash: {key.PubKey.Hash.ScriptPubKey}");
 
-
+            ////
             // why P2PKH is used:
+            ////
 
             // 1. ECC is vulnerable to a modified Shor's algorithim for solving the discrete logarithim problem on ECs
             // (quantum computers retrieving private keys from public keys)
@@ -56,8 +59,9 @@ namespace OtherTypesOfOwnership
 
             // 2.as the hash is smaller (20 bytes), it's easier to embed into small storage units, e.g., QR codes
 
-
+            ////
             // P2WPKH  (pay to witness public key hash)
+            ////
 
             // the signature contains same info as P2PKH spend
             // but is located in the witness instead of the scriptSig
@@ -70,7 +74,55 @@ namespace OtherTypesOfOwnership
             var key2 = new Key();
             Console.WriteLine(key2.PubKey.WitHash.ScriptPubKey);
 
+            ////
+            // multi sig
+            ////
 
+            var alice = new Key();
+            var bob = new Key();
+            var satoshi = new Key();
+
+            var multiScriptPubKey = PayToMultiSigTemplate
+                .Instance
+                .GenerateScriptPubKey(2, bob.PubKey, alice.PubKey, satoshi.PubKey); // 2 sig required
+
+            Console.WriteLine($"multi sig:\n{multiScriptPubKey}");
+
+            // Transaction.Sign does not work for multi sig
+
+            // multi sig received a coin in a tx:
+            var received = new Transaction();
+            received.Outputs.Add(new TxOut(Money.Coins(1.0m), scriptPubKey));
+
+            // bob and alice agree to pay nico 1.0 btc
+
+            // first get the coin
+            var coin = received.Outputs.AsCoins().First();
+
+            // create unsigned tx
+            var nicosAddress = new Key().PubKey.GetAddress(Network.Main);
+            var builder = new TransactionBuilder();
+
+            Transaction unsigned = builder
+                .AddCoins(coin)
+                .Send(nicosAddress, Money.Coins(1.0m))
+                .BuildTransaction(sign: false);
+
+            // alice signs
+            Transaction aliceSigned = builder
+                .AddCoins(coin)
+                .AddKeys(alice)
+                .SignTransaction(unsigned);
+
+            // bob signs
+            Transaction bobSigned = builder
+                .AddCoins(coin)
+                .AddKeys(bob)
+                // at this point,
+                // SignTransaction(unsigned) has the identical functionality with the SignTransaction(aliceSigned)
+                // because unsigned transaction has alread been signed by Alice priv key above
+                .SignTransaction(aliceSigned);
+            
 
             Console.ReadLine();
         }
